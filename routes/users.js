6d85206f005a687c9b7ca5666cb992cc1ac29bc8
CastilloTech <CastilloTech@gmail.com>
2020-09-02 00:08:15 -0400

    User Route Controller

--- routes/users.js
+++ routes/users.js
@@ -1,3 +1,16 @@
-app.get('/', (req, res) => {
-  res.status(200).json({success:true, msg:"Show all users"});
-})
+const express = require('express');
+const {
+  getUsers,
+  getUser,
+  createUser,
+  updateUser,
+  deleteUser,
+} = require('../controllers/users');
+
+const router = express.Router();
+
+router.route('/').get(getUsers).post(createUser);
+
+router.route('/:id').get(getUser).put(updateUser).delete(deleteUser);
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const dotenv = require('dotenv');
 
+//Route files
+const users = require('./routes/users');
+
 //Load env vars
 dotenv.config({ path: './config/config.env' });
 
 const app = express();
+
+//Mount routers
+app.use('/api/v1/users', users);
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(
